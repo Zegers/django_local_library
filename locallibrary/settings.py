50ab017f1160342b404af4301923a3c828610cd8
jonathanZeg <zegers.jonathan37@gmail.com>
2018-01-01 14:54:58 -0500

    Removed https:// from allowed hosts, added localhost

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','v+jpfc%nou20lhsm148372ful^ru^p#
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['https://warm-retreat-60002.herokuapp.com/',]
+ALLOWED_HOSTS = ['warm-retreat-60002.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
