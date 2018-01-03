c8ba034c9720830d2f09b0cd02ddecc5f0aa0f43
jonathanZeg <zegers.jonathan37@gmail.com>
2018-01-02 20:06:10 -0500

    added postgres support

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_postgres_Db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
